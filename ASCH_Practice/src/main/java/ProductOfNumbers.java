9cd7dafed98d4d5fd753f5e15210422e9ea5f25c
S2KB4X <Pavel_Yeremeev@rcomext.com>
2019-08-26 17:34:12 +0300

    - fixed design of the code

--- ASCH_Practice/src/main/java/ProductOfNumbers.java
+++ ASCH_Practice/src/main/java/ProductOfNumbers.java
@@ -1,11 +1,12 @@
 
 
 public class ProductOfNumbers {
-        public long productOfInput(int a, int b) {
-            long dev = 1;
-            for(int i = a ; i < b ; i++){
-                dev = dev*i;
-            } return dev;
+    public long productOfInput(int a, int b) {
+        long dev = 1;
+        for (int i = a; i < b; i++) {
+            dev = dev * i;
         }
+        return dev;
     }
+}
 
--- ASCH_Practice/src/test/java/ProductOfNumbersTests.java
+++ ASCH_Practice/src/test/java/ProductOfNumbersTests.java
@@ -16,19 +16,22 @@ public class ProductOfNumbersTests {
 
     @Test
     public void productOfNumbersSmall() {
-        assertEquals(1, ProductNumbers.productOfInput(1,2));
+        assertEquals(1, ProductNumbers.productOfInput(1, 2));
     }
+
     @Test
     public void productOfNumbersBig() {
-        assertEquals(11035502400.0, ProductNumbers.productOfInput(100,105));
+        assertEquals(11035502400.0, ProductNumbers.productOfInput(100, 105));
     }
+
     @Test
     public void productOfNumbersZero() {
-        assertEquals(0, ProductNumbers.productOfInput(0,1));
+        assertEquals(0, ProductNumbers.productOfInput(0, 1));
     }
+
     @Test
     public void productOfNumbersMaximumInt() {
-        assertEquals(1.10355024E10, ProductNumbers.productOfInput(1,999999999));
+        assertEquals(1.10355024E10, ProductNumbers.productOfInput(1, 999999999));
     }
 
 
