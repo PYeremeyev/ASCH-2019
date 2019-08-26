d01c86f9d8d8d5edb2b2bac9abb8428c078f10b9
S2KB4X <Pavel_Yeremeev@rcomext.com>
2019-08-26 17:29:30 +0300

    - Product of numbers (integers) program added - Product of numbers tests added - Theory screenshot added

---
+++ ASCH_Practice/src/main/java/ProductOfNumbers.java
@@ -0,0 +1,11 @@
+
+
+public class ProductOfNumbers {
+        public long productOfInput(int a, int b) {
+            long dev = 1;
+            for(int i = a ; i < b ; i++){
+                dev = dev*i;
+            } return dev;
+        }
+    }
+
