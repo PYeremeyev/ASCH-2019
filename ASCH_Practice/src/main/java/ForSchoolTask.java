263b68dabacb66224154e44d9e60a8751c3348d7
S2KB4X <Pavel_Yeremeev@rcomext.com>
2019-08-27 17:00:47 +0300

    -ForSchoolTask program introduced -ForSchoolTask tests introduced

---
+++ ASCH_Practice/src/main/java/ForSchoolTask.java
@@ -0,0 +1,15 @@
+public class ForSchoolTask {
+
+    public static int countTwoArrays(int a, int b, int c, int d){
+        int count = 0;
+        for(int i = a; i <=b; i++){
+          if(i < d & i > c){
+              count++;
+          }
+        }return count;
+
+    }
+
+
+
+}
---
+++ ASCH_Practice/src/test/java/ForSchoolTaskTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ForSchoolTaskTests {
+
+    @Test
+    public void happyPath() {
+        assertEquals(5, ForSchoolTask.countTwoArrays(0, 10, 1, 7));
+    }
+
+    @Test
+    public void invalidRange() {
+        assertEquals(0, ForSchoolTask.countTwoArrays(0, 10, -10, -1));
+    }
+
+    @Test
+    public void maximumInteger() {
+        assertEquals(0, ForSchoolTask.countTwoArrays(99999999, 999999999, 999999999, 999999999));
+    }
+
+
+}
+
+
+
